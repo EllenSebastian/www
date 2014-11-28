f7794aeecf7bd22abb9fbd10dea4685a379310cb
Ellen Sebastian <ellen.sebastian@healthtap.com>
2014-11-28 02:27:07 -0600

    added view-space.html; guest tasks done

---
+++ img/Facebook_files/VroBNnLknL2.js
@@ -0,0 +1,6 @@
+/*!CK:485251053!*//*1416801260,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["CqQSf"]); }
+
+__d("AttachmentCTA",["Style"],function(a,b,c,d,e,f,g){var h=10,i={init:function(j){var k=j.parentNode,l=Math.max(k.scrollHeight-j.scrollHeight-h*2,0);g.set(j,'margin-top',l+'px');}};e.exports=i;},null);
+__d("XDeveloperExplorerSessionAsyncEndpointControllerURIBuilder",["XControllerURIBuilder"],function(a,b,c,d,e,f){e.exports=b("XControllerURIBuilder").create("\/tools\/explorer\/session\/",{});},null);
